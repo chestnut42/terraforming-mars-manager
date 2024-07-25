66aa339ea238f7f1ea51867a3fc3ce3af02d1b60
Andrei <andrey.makarich@gmail.com>
2024-07-25 23:45:21 +0200

    test config

--- cmd/manager/config_test.go
+++ cmd/manager/config_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -9,4 +10,17 @@ import (
 func TestNewConfig(t *testing.T) {
 	_, err := NewConfig()
 	assert.NilError(t, err)
+}
+
+func TestConfig(t *testing.T) {
+	err := os.Setenv("MARS_LISTEN", ":42")
+	assert.NilError(t, err)
+
+	err = os.Setenv("MARS_GAME_URL", "https://example.com/url")
+	assert.NilError(t, err)
+
+	c, err := NewConfig()
+	assert.NilError(t, err)
+	assert.Equal(t, c.Listen, ":42")
+	assert.Equal(t, c.GameURL.String(), "https://example.com/url")
 }
